fc4432ca1ac2b865c02af977afb57e6b35ca7ed7
Nithika-124 <nithikanimlaka93@gmail.com>
2026-05-14 10:48:05 +0530

    MyTrip page design

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { LandingPage } from './pages/LandingPage';
 import { Dashboard } from './pages/Dashboard';
 import { AppLayout } from './components/NavBar';
 import { Explore } from './pages/Explore';
+import { MyTrips } from './pages/MyTrips';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -24,6 +25,7 @@ function App() {
         <Route element={<AppLayout />}>
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/explore' element={<Explore />} />
+          <Route path='/my-trips' element={<MyTrips />} />
         </Route>
       </Routes>
     </BrowserRouter>
